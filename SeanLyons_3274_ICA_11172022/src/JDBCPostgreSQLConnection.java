d8b53bcb7fb16c98981f3756763f4c6c3f76c217
otimu <otimus1115@gmail.com>
2022-11-29 11:18:19 -0600

    11/29/2022 Chapter 24

--- SeanLyons_3274_ICA_11172022/src/JDBCPostgreSQLConnection.java
+++ SeanLyons_3274_ICA_11172022/src/JDBCPostgreSQLConnection.java
@@ -1,12 +1,16 @@
 
 /**
- * @author Sean Lyons
+ * @author Sean Lyons 
  * Date: 11/17/2022
+ * 11/29/2022: create a table & insert a row
  */
 
 import java.sql.Connection;
+
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class JDBCPostgreSQLConnection {
 	// private final String url = "jdbc:postgresql://localhost/myDB";
@@ -43,6 +47,27 @@ public class JDBCPostgreSQLConnection {
 	 */
 	public static void main(String[] args) {
 		JDBCPostgreSQLConnection app = new JDBCPostgreSQLConnection();
-		app.connect();
+		Connection conn = app.connect();
+		int count = 0;
+
+		try {
+			Statement stat = conn.createStatement();
+			stat.execute("drop table test");
+			stat.execute("CREATE TABLE Test(Name VARCHAR(20))");
+			for (int i = 0; i < 10; i++) {
+				stat.execute("INSERT INTO Test VALUES('Romeo')");
+			}
+
+			ResultSet result = stat.executeQuery("SELECT * FROM Test");
+
+			while (result.next()) {
+				System.out.println(result.getString("Name"));
+				count++;
+			}
+			System.out.print(count + "(rows)");
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+
 	}
 }
